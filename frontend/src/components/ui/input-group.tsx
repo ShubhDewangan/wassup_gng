9f0cd86b320e115918fb1a985198a69d53d25d02
Shubh Dewangan <shubhdwngn24@gmail.com>
2026-07-30 14:21:19 +0530

    updated wrong imports

--- frontend/src/components/ui/input-group.tsx
+++ frontend/src/components/ui/input-group.tsx
@@ -2,9 +2,9 @@ import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "../../lib/utils"
-import { Button } from "../../lib/utils"
-import { Input } from "../../lib/utils"
-import { Textarea } from "../../lib/utils"
+import { Button } from "./button"
+import { Input } from "./input"
+import { Textarea } from "./textarea"
 
 function InputGroup({ className, ...props }: React.ComponentProps<"div">) {
   return (
